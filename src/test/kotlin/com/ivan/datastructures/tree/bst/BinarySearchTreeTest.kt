ad68b3f5b6597126e2e841c2509d4cbe2cfddc14
Ivan <ivan.stankov1992@gmail.com>
2019-04-04 19:23:50 +0300

    Added test library

---
+++ src/test/kotlin/com/ivan/datastructures/tree/bst/BinarySearchTreeTest.kt
@@ -0,0 +1,17 @@
+package com.ivan.datastructures.tree.bst
+
+import kotlin.test.Test
+
+internal class BinarySearchTreeTest {
+
+
+
+    fun insert_WhenTreeDoesNotHaveRoot_ShouldInsertNewRoot() {
+
+    }
+
+    @Test
+    fun find() {
+    }
+
+}
